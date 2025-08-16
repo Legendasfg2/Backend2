944593fc67c8afa9ae67f21db8f33dff3c96b7e8
Natan Bugrinsky <natanbugtinskiy2008@gmail.com>
2025-08-16 18:36:54 +0500

    Sum

--- lesson1.py
+++ lesson1.py
@@ -1,1 +1,5 @@
-print("Hello, Backend!")
+numbers = input("Введите числа через пробел: ")
+numbers = list(map(int, numbers.split()))  # преобразуем ввод в список чисел
+total = sum(numbers)
+
+print("Сумма чисел =", total)
