740151eeeeff026e293ac1160abda552105e6908
Legendasfg2 <natanbugtinskiy2008@gmail.com>
2025-08-22 21:10:23 +0500

    Nechot

--- Python/Test/a.py
+++ Python/Test/a.py
@@ -1,3 +1,3 @@
 avp = input("Введите строку: ")
-count = avp.count('а') + avp.count('a')
+count = avp.count("а") + avp.count("a")
 print(f"Количество букв а: {count}")
---
+++ Python/Test/nechot.py
@@ -0,0 +1,5 @@
+num = int(input("Введите число: "))
+print(f"Нечетные числа от 1 до {num}:")
+for i in range(1, num + 1):
+    if i % 2 != 0:
+        print(i)
