d321bfe953e8bf31d3eba11a9eb381b3dcb7822e
Legendasfg2 <natanbugtinskiy2008@gmail.com>
2025-09-07 15:56:36 +0500

    p

---
+++ Python/Lessons/list/1.py
@@ -0,0 +1,8 @@
+films = []
+for i in range (1, 6):
+    film = input("Введите название фильма: ")
+    films.append(film)
+print(films)
+
+
+
---
+++ Python/Lessons/list/2.py
@@ -0,0 +1,6 @@
+num1 = [float(input("Введите первое число: "))]
+num2 = [float(input("Введите второе число: "))]
+num3 = [float(input("Введите третье число: "))]
+num = [num1, num2, num3]
+num.sort()
+print(num)
